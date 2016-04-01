1c72bd2c92f87199e471acf9c081a2c10c938957
iankantian <iankantian@gmail.com>
2016-04-01 14:12:31 -0700

    got Vast working!
    Now need to spice it up with other ads and other vids…

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,124 +1,77 @@
 'use strict';
 
-module.exports = function(grunt) {
+var specs = "spec/*Spec.js",
+    helpers = "spec/*Helpers.js";
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    // Task configuration.
-    clean: {
-      files: ['dist']
-    },
-    copy: {
-      global: {
-        src: 'src/videojs.ads.js',
-        dest: 'dist/videojs.ads.global.js'
-      }
-    },
-    concat: {
+var _ = require('lodash');
+
+var gruntConfig = {
+  env: {
+    // dynamically filled
+  },
+  jshint: {
+    ignore_warning: {
       options: {
-        banner: '<%= banner %>',
-        stripBanners: true
+        '-W083': true,
       },
-      dist: {
-        src: ['dist/videojs.ads.js'],
-        dest: 'dist/videojs.ads.js'
-      },
-      global: {
-        src: ['dist/videojs.ads.global.js'],
-        dest: 'dist/videojs.ads.global.js'
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          keepalive: true
-        }
-      }
-    },
-    umd: {
-      all: {
-        options: {
-          src: 'src/videojs.ads.js',
-          dest: 'dist/videojs.ads.js',
-          template: 'umd',
-          amdModuleId: 'videojs-contrib-ads',
-          globalAlias: 'videojs-contrib-ads',
-          deps: {
-            default: ['videojs'],
-            amd: ['videojs'],
-            cjs: ['video.js']
-          }
-        }
-      }
-    },
-    uglify: {
+      src: ['videojs.vast.js', specs],
+    }, 
+  },
+  // jasmine: {
+  //   src: 'videojs.vast.js',
+  //   options: {
+  //     specs: specs,
+  //     helpers: helpers,
+  //     vendor: [
+  //       "http://vjs.zencdn.net/4.4.3/video.js",
+  //       "bower_components/videojs-contrib-ads/src/videojs.ads.js",
+  //       "lib/vast-client.js"
+  //     ]
+  //   }
+  // },
+  karma: {
+    unit: {
+      configFile: 'karma.conf.js'
+    }
+  },
+  coveralls: {
+    options: {
+      coverage_dir: './coverage',
+      force: true,
+      recursive: true
+    }
+  },
+  connect: {
+    server: {
       options: {
-        banner: '<%= banner %>'
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/videojs.ads.min.js'
-      },
-      global: {
-        src: '<%= concat.global.dest %>',
-        dest: 'dist/videojs.ads.global.min.js'
-      },
+        keepalive: true
+      }
+    }
+  },
+  watch: {
+    scripts: {
+      files: ['*.js', 'spec/*.js'],
+      tasks: ['jshint', 'jasmine']
     },
-    qunit: {
-      files: ['test/**/*.html', '!test/events.html']
-    },
-    jshint: {
-      gruntfile: {
-        options: {
-          jshintrc: '.jshintrc'
-        },
-        src: 'Gruntfile.js'
-      },
-      src: {
-        options: {
-          jshintrc: 'src/.jshintrc'
-        },
-        src: ['src/**/*.js']
-      },
-      test: {
-        options: {
-          jshintrc: 'test/.jshintrc'
-        },
-        src: ['test/**/*.js']
-      },
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      src: {
-        files: '<%= jshint.src.src %>',
-        tasks: ['jshint:src', 'qunit']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'qunit']
-      },
-    },
+  }
+};
+
+module.exports = function(grunt) {
+  grunt.initConfig(gruntConfig);
+
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-karma-coveralls');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('default', ['test:sauce:' + _(desireds).keys().first()]);
+
+  _(desireds).each(function(desired, key) {
+    grunt.registerTask('test:sauce:' + key, ['env:' + key, 'simplemocha:sauce']);
   });
 
-  // These plugins provide necessary tasks.
-  require('load-grunt-tasks')(grunt);
-
-  grunt.registerTask('build', ['clean', 'copy', 'umd', 'concat', 'uglify']);
-
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'build']);
-
-  // travis build task
-  grunt.registerTask('build:travis', ['jshint', 'test:node']);
-
+  grunt.registerTask('default', ['jshint', 'karma']);
 };
